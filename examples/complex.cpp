21d3c0cd5ed5131dec03c69c6ac24de6d72407fa
Ab Wilson <norman.wilson@gmail.com>
2015-07-01 11:53:01 +0100

    Add complex example.
    Topology includes multiple producers, fan out and fan back in, with one
    disruptor feeding into another one.

---
+++ examples/complex.cpp
@@ -0,0 +1,179 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Norman Wilson - Volcano Consultancy Ltd
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#include <L3/disruptor/disruptor.h>
+
+#include <thread>
+#include <iostream>
+
+//
+// We're going to set up the following topology.
+//
+// P1
+//   \
+//    D1 - C1
+//   /  \    \
+// P2     C2 - C3/P3 - D2 - C4
+//
+// Producers P1 and P2 feed into disruptor D1. Consumers C1 and C2 get
+// directly from the ring. Consumer C3 follows C1 and C2 and passes
+// the messages into the second disruptor D2. These are finally
+// consumed by C4.
+//
+// This might be a complex topology but we're still not going to do
+// anything realistic with the messages. Still passing numbers around
+// to make it easy to test no messages are lost and order is
+// preserved.
+//
+using Msg = size_t;
+//
+// Size of ring buffers as power of two. I've chosen this number
+// because 2^17 == 128K and 128K * sizeof(Msg) == 1MB.
+//
+constexpr size_t log2size = 17;
+//
+// First disruptor handles these messages. Third template parameter is
+// an instance tag. It defaults to 0 but I'm supplying 1 here
+// explicitly since we're defining D1.
+//
+using D1 = L3::Disruptor<Msg, log2size, 1>;
+//
+// We must define Get types for our consumers first. C1 and C2 follow
+// the commit cursor in D1. This is the default. Need to supply
+// explicit instance number to C2 but I've give them for both to make
+// it explicit.
+//
+using Get1 = D1::Get<1>;
+using Get2 = D1::Get<2>;
+//
+// C3 follows C1 and C2.
+//
+using Get3 = D1::Get<3, L3::Barrier<Get1, Get2>>;
+//
+// Define the Put type shared between P1 and P2. It follows C3. It
+// uses a shared commit policy.
+//
+using Put1 = D1::Put<L3::Barrier<Get3>, L3::CommitPolicy::Shared>;
+//
+// Now define the second disruptor much like the first.
+//
+using D2 = L3::Disruptor<Msg, log2size, 2>;
+//
+// It is single producer/single consumer which is what the default
+// Put/Get templates do.
+//
+using Get4 = D2::Get<>;
+using Put2 = D2::Put<>;
+//
+// Going to produce sequences of numbers. P1 odd, P2 even. We reserve
+// 0 to as and end of stream message to shut things down.
+//
+constexpr Msg eos = 0;
+template<typename Put>
+void
+produce(Msg first, Msg last)
+{
+    for(Msg i = first; i < last; i += 2)
+    {
+        Put() = i;
+    }
+    Put() = eos;
+}
+template<typename Get, typename F>
+void
+consume(const F& f)
+{
+    int endCount = 0;
+    
+    for(;;)
+    {
+        for(auto msg: Get())
+        {
+            f(msg);
+
+            if(msg == eos && ++endCount < 2)
+            {
+                return;
+            }
+        }
+    }
+}
+//
+// Consumers make sure sequence of messages is correct.
+//
+template<typename Get>
+void
+checkSequence()
+{
+    Msg oldOdd = 1;
+    Msg oldEven = 0;
+
+    consume<Get>(
+        [&](Msg msg)
+        {
+            if(msg == eos)
+            {
+                return;
+            }
+            Msg& old = msg & 0x1L ? oldOdd : oldEven;
+            if(msg != old + 2)
+            {
+                std::cout << "FAIL: old: " << old << ", new: " << msg
+                          << std::endl;
+            }
+            old = msg;
+        });
+}
+//
+// C3 acts as a bridge between D1 and D2.
+//
+void consumer3Producer2()
+{
+    consume<Get3>([](Msg msg){ Put2() = msg; });
+}
+
+int
+main()
+{
+    //
+    // Stick it all together.
+    //
+    Msg max = 10000000;
+    std::thread p1([&]{ produce<Put1>(3, max); });
+    //
+    // Second producer sends end signal.
+    //
+    std::thread p2([&]{ produce<Put1>(2, max); });
+    std::thread c1(checkSequence<Get1>);
+    std::thread c2(checkSequence<Get2>);
+    std::thread c3(consumer3Producer2);
+    std::thread c4(checkSequence<Get4>);
+    //
+    // We love c++11.
+    //
+    for(auto t: {&p1, &p2, &c1, &c2, &c3, &c4})
+    {
+        t->join();
+    }
+}
+    
